1e8550b85077a1aabdaccf73aa05fe1372b7cf1e
ayesha <ayesha@ayesha.local>
2025-05-25 23:33:24 +0500

    var & val variables

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,10 @@
 fun main () {
-    print("Hello World")
-    greeting()
-}
+   var age = 25
+    println(age)
+    println(age+1)
+    age = 30
+    println(age)
 
-fun greeting() {
-    print("Good Morning")
+    val ageVal = 25
+    println(ageVal)
 }
